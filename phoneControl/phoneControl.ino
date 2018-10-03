fc190a4249aba6bd6b84ca906a8909799caac641
biomotion <bensonpuppy1997@gmail.com>
2018-10-04 01:34:39 +0800

    phoneControl

--- phoneControl/phoneControl.ino
+++ phoneControl/phoneControl.ino
@@ -1,8 +1,9 @@
 #define LATCH_PIN 12
 #define CLOCK_PIN 11
 #define DATA_PIN 8
+#define NUM_OF_ROWS 24
 
-#define data 0b10101010
+byte table[24][3] = { 0 };
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,9 +16,17 @@ void loop() {
   // put your main code here, to run repeatedly:
   for(int i=0; i<8; i++){
     digitalWrite(LATCH_PIN, LOW);
-    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, (data>>i)|(data<<(8-i)));
+//    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, (data>>i)|(data<<(8-i)));
     digitalWrite(LATCH_PIN, HIGH);
     delay(500);
   }
-
 }
+
+void showOneRow(byte data[]){
+  digitalWrite(LATCH_PIN, LOW);
+  for(byte i=0; i<3; i++){
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, data[i]);
+  }
+  digitalWrite(LATCH_PIN, HIGH);
+}
+
