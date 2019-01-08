b6e60d9de39c59159c8fd8a102f79c74183da75b
biomotion <35159458+biomotion@users.noreply.github.com>
2019-01-09 00:08:49 +0800

    Update phoneControl.ino

--- phoneControl/phoneControl.ino
+++ phoneControl/phoneControl.ino
@@ -10,60 +10,37 @@
 #define DATA_PIN 8    // to pin 8
 #define NUM_OF_ROWS 24
 #define NUM_OF_REGS 2
-const byte ascii2hex[103] = { 0,0,0,0,0,0,0,0,0,0,
-                              0,0,0,0,0,0,0,0,0,0,
-                              0,0,0,0,0,0,0,0,0,0,
-                              0,0,0,0,0,0,0,0,0,0,
-                              0,0,0,0,0,0,0,0,0,1,
-                              2,3,4,5,6,7,8,9,0,0,
-                              0,0,0,0,0,10,11,12,13,14,
-                              15,0,0,0,0,0,0,0,0,0,
-                              0,0,0,0,0,0,0,0,0,0,
-                              0,0,0,0,0,0,0,10,11,12,
-                              13,14,15,};
+
 SoftwareSerial BTserial(RX, TX);
-const uint16_t baudRate = 38400;
+const uint16_t baudRate = 9600;
 void displayTable(int delayTime=2);
-byte table[NUM_OF_ROWS][NUM_OF_REGS] = { 0 };
+byte showingTable[NUM_OF_ROWS][NUM_OF_REGS] = { 0 };
 void setup() {
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(DATA_PIN, OUTPUT);
-  Serial.begin(38400);
+  Serial.begin(9600);
   BTserial.begin(baudRate);
   Serial.print("BTserial started at "); Serial.println(baudRate);
   Serial.println(" ");
 }
 
 void loop() {
-    displayTable();
+    //displayTable();
     // Read from the Bluetooth module and send to the Arduino Serial Monitor
     if (BTserial.available())
     {
-        for(uint8_t i=0; i<NUM_OF_ROWS; i++){
-          for(uint8_t j=0; j<NUM_OF_REGS; j++){
-            while(BTserial.available()<2){}
-            table[i][j] = readByte();
-          }
-        }
+       delay(50);
+       //Serial.println((uint8_t)readByte());
+       Serial.println(readAll().toInt());
     }
  
 }
 
-byte readByte(){
-  
-  byte result;
-  while(!BTserial.available()){}
-  result = ascii2hex[BTserial.read()];
-  result << 4;
-  while(!BTserial.available()){}
-  result += ascii2hex[BTserial.read()];
-  return result;
-}
 
 void displayTable(int delayTime=2){
   for(byte i=0; i<NUM_OF_ROWS; i++){
-    showOneRow(table[i]);
+    showOneRow(showingTable[i]);
     delay(delayTime);
   }
 }
