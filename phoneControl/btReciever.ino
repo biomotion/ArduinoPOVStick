73bef2a4c451d14221f3fa124a39ec27cf58c376
biomotion <35159458+biomotion@users.noreply.github.com>
2019-01-16 23:52:42 +0800

    Update btReciever.ino

--- phoneControl/btReciever.ino
+++ phoneControl/btReciever.ino
@@ -23,7 +23,7 @@ byte readByte(){
 }
 
 String readString(){
-  const uint16_t bufferLen = 1000;
+  const uint16_t bufferLen = 100;
   char buffer[bufferLen] = {0};
   if(!BTserial.available()) return;
   for(uint16_t i=0; i<bufferLen; i++){
@@ -35,7 +35,24 @@ String readString(){
       break;
     }
   }
+  
   Serial.println(buffer);
   return String(buffer);
+}
+
+void readMessage(String message){
+  int current=0, next;
+  int i=0;
+  while((next = message.indexOf(',', current))!=-1){
+    showingTable[i++] = message.substring(current, next).toInt();
+    Serial.println(message.substring(current, next).toInt());
+    current = next+1;
+    if(i >= NUM_OF_ROWS) break;
+  }
+  if(i < NUM_OF_ROWS){
+    showingTable[i++] = message.substring(current, next).toInt();
+    Serial.println(message.substring(current, next).toInt());
+  }
+  
 }
 
