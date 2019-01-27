ffc9759b842136d5378f3cde1103107045b9102e
biomotion <bensonpuppy1997@gmail.com>
2019-01-27 22:19:23 +0800

    init 74595

--- setBTName/setBTName.ino
+++ setBTName/setBTName.ino
@@ -7,40 +7,67 @@ boolean NL = true;
  
 void setup() 
 {
-    Serial.begin(9600);
-    Serial.print("Sketch:   ");   Serial.println(__FILE__);
-    Serial.print("Uploaded: ");   Serial.println(__DATE__);
-    Serial.println(" ");
+	
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(8, OUTPUT);
+  digitalWrite(12, LOW);
+  for(byte i=0; i<2; i++){
+    shiftOut(8, 11, MSBFIRST, 0);
+  }
+  digitalWrite(12, HIGH);
+  
+  Serial.begin(9600);
+  Serial.print("Sketch:   ");   Serial.println(__FILE__);
+  Serial.print("Uploaded: ");   Serial.println(__DATE__);
+  Serial.println(" ");
  
-    BTserial.begin(baudRate);  
-    Serial.print("BTserial started at "); Serial.println(baudRate);
-    Serial.println(" ");
+  BTserial.begin(baudRate);  
+  Serial.print("BTserial started at "); Serial.println(baudRate);
+  Serial.println(" ");
 }
  
 void loop()
 {
-  sync();
+  String newName = "";
+  Serial.println("AT initiating...");
+  BTserial.println("AT");
+  delay(500);
+  readBT();
+
+  BTserial.println("AT+NAME?");
+  delay(200);
+  readBT();
+
+  Serial.println("Enter BT name: ");
+  while(!Serial.available());
+  delay(50);
+  while(Serial.available()){
+    char c = Serial.read();
+    if(c == 10)
+      break;
+    newName += c;
+  }
+  Serial.print("Setting BT name to ");
+  Serial.println(newName + "...");
+  BTserial.println("AT+NAME=" + newName);
+  delay(200);
+  readBT();
+
+  Serial.println("Getting address...");
+  BTserial.println("AT+ADDR?");
+  delay(200);
+  readBT();
+  BTserial.println("AT+NAME?");
+  delay(200);
+  readBT();
+
+  while(1);
+}
+void readBT(){
+  while(BTserial.available()){
+    c = BTserial.read();
+    Serial.write(c);
+  }
 }
 
-void sync(){
-    // Read from the Bluetooth module and send to the Arduino Serial Monitor
-    if (BTserial.available())
-    {
-        c = BTserial.read();
-        Serial.write(c);
-    }
- 
- 
-    // Read from the Serial Monitor and send to the Bluetooth module
-    if (Serial.available())
-    {
-        c = Serial.read();
-        BTserial.write(c);   
- 
-        // Echo the user input to the main window. The ">" character indicates the user entered text.
-        if (NL) { Serial.print(">");  NL = false; }
-        Serial.write(c);
-        if (c==10) { NL = true; }
-    }
- 
-}
